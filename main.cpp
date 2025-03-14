754403e61aa7889ba87b9864148d39e0a4b9aab3
Дьяконов Иван <hgfgvvg.ru@gmail.com>
2025-03-14 23:49:02 +0300

    Added numbers search

--- main.cpp
+++ main.cpp
@@ -30,77 +30,80 @@ int newarray_task3(int *array, int size, int chet, int nechet) {
 };
 
 int main() {
-    float arry[7];
-    int i {0};
+    int task;
+    cout << "Enter the task number: ";
+    cin >> task;
+    if (task == 1 or task == 2) {
+        float arry[7];
+        int i {0};
 
-    while (i<7) {
-        cout << "Enter a number "<< i+1 <<": ";
-        cin >> arry[i];
-        i++;
-    }
-    for (int i {0}; i < 7; i++) {
-        cout << arry[i] << " ";
-    }
-    int povtor = 0;
-    for (int i = 0; i < 7; i++) {
-        for (int j = i + 1; j < 7; j++) {
-            if (arry[i] == arry[j]) {
-                povtor++;
-                break;
+        cout << "Task 1-2" << endl;
+        while (i<7) {
+            cout << "Enter a number "<< i+1 <<": ";
+            cin >> arry[i];
+            i++;
+        }
+        for (int i {0}; i < 7; i++) {
+            cout << arry[i] << " ";
+        }
+        int povtor = 0;
+        for (int i = 0; i < 7; i++) {
+            for (int j = i + 1; j < 7; j++) {
+                if (arry[i] == arry[j]) {
+                    povtor++;
+                    break;
+                }
             }
         }
-    }
-    cout << "" << endl;
-    cout << "Povtor: " << povtor << endl;
+        cout << "" << endl;
+        cout << "Povtor: " << povtor << endl;
 
 
-    float *max=&arry[0];
-    float *min=&arry[0];
-    for (int i {1}; i < 7; i++) {
-        if (*max < arry[i]) {
-            max = &arry[i];
+        float *max=&arry[0];
+        float *min=&arry[0];
+        for (int i {1}; i < 7; i++) {
+            if (*max < arry[i]) {
+                max = &arry[i];
+            }
         }
-    }
 
-    cout << "Max: " << *max << endl;
-
+        cout << "Max: " << *max << endl;
 
 
 
-    for (int i {1}; i < 7; i++) {
-        if (*min > arry[i]) {
-            min = &arry[i];
+
+        for (int i {1}; i < 7; i++) {
+            if (*min > arry[i]) {
+                min = &arry[i];
+            }
         }
-    }
-    cout << "Min: " << *min << endl;
-    *max = 0;
-    *min = 0;
-    cout << "Magic......." <<endl;
-    cout << "Max: " << *max << endl;
-    cout << "Min: " << *min << endl;
+        cout << "Min: " << *min << endl;
+        *max = 0;
+        *min = 0;
+        cout << "Magic......." <<endl;
+        cout << "Max: " << *max << endl;
+        cout << "Min: " << *min << endl;
+    } else {
+        cout <<  "Task 3" << endl;
+        cout << "Dynamic arry length: ";
+        int x{};
+        int chet{0};
+        int nechet{0};
 
-
-
-
-    cout <<  "Task 3" << endl;
-    cout << "Dynamic arry length: ";
-    int x{};
-    int chet{0};
-    int nechet{0};
-
-    cin >> x;
-    int *dynamic_arry = new int[x];
-    for (int i {0}; i < x; i++) {
-        cout << "Enter a number "<< i+1 <<": ";
-        cin >> dynamic_arry[i];
-        if (dynamic_arry[i]%2 == 0) {
-            chet +=1;
-        } else {
-            nechet +=1;
+        cin >> x;
+        int *dynamic_arry = new int[x];
+        for (int i {0}; i < x; i++) {
+            cout << "Enter a number "<< i+1 <<": ";
+            cin >> dynamic_arry[i];
+            if (dynamic_arry[i]%2 == 0) {
+                chet +=1;
+            } else {
+                nechet +=1;
+            }
         }
+        newarray_task3(dynamic_arry, x, chet, nechet);
     }
 
-    newarray_task3(dynamic_arry, x, chet, nechet);
 
 
     return 0;
